1cc818bd51d8390c531c4bf9bc9041c3cc01f09b
Sam Itschner <swizrc@mst.edu>
2017-09-14 16:18:31 -0500

    -Replaced original data model of a dictionary with a standard array. This made implementation a lot easier, the dictionary model was too cumbersome for this application. Perhaps another time. -Names are sorted alphabetically in the table and user data model. -Ran into a sorting bug. Usernames are sorted alphabetically, but lower cased names were always put at the bottom after capitalized names. All names are forced capitalized now. -Added functionality for adding another user to the table. User can input their name and an optional picture.

---
+++ FoodTracker/UserDetailViewController.swift
@@ -0,0 +1,82 @@
+//
+//  UserDetailViewController.swift
+//  FoodTracker
+//
+//  Created by Samuel Itschner on 9/13/17.
+//  Copyright © 2017 Samuel Itschner. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var subView: UIView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userImageView: UIImageView!
+    var sendingUser: UserIdentData = UserIdentData(user: "temp",userPic: nil)!
+    var UserData = [UserIdentData]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTextField.delegate = self
+        userImageView.isUserInteractionEnabled = true
+        subView.isUserInteractionEnabled = true
+        let tapRecognizer = UITapGestureRecognizer(target: self,action: #selector(selectImageFromPhotoAlbum))
+        tapRecognizer.delegate = self
+        userImageView.addGestureRecognizer(tapRecognizer)
+    }
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+        var Unique:Bool = true
+        for elem in UserData{
+            if elem.user == userNameTextField.text{
+                Unique = false
+            }
+        }
+        if userNameTextField.text != "" && Unique == true{
+            sendingUser.user = userNameTextField.text!
+            sendingUser.userPic = userImageView.image
+            self.performSegue(withIdentifier: "unwindToUserTable", sender: self)
+        }
+    }
+
+    @IBAction func CancelButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func selectImageFromPhotoAlbum(sender: UITapGestureRecognizer? = nil) {
+        userNameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .savedPhotosAlbum
+        imagePickerController.delegate = self
+        present(imagePickerController,animated: true,completion: nil)
+    }
+
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        userImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    internal func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FoodTracker/UserTableViewController.swift
+++ FoodTracker/UserTableViewController.swift
@@ -10,26 +10,29 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
     
-    var UserData = [UserIdentData:[DateData]]()
-    var order = [String]()
+    var UserData = [UserIdentData]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = self.editButtonItem
         self.navigationItem.title = "Select a User"
         loadSampleUser()
+        SortUsers()
     }
     
     private func loadSampleUser(){
         let user1 = UserIdentData(user: "Xess",userPic: #imageLiteral(resourceName: "defaultPhoto"))
-        let user2 = UserIdentData(user: "Sam",userPic: #imageLiteral(resourceName: "eggs-pan"))
-        UserData[user1!] = [DateData(date:"")]
-        UserData[user2!] = [DateData(date:"1")]
+        let user2 = UserIdentData(user: "Pam",userPic: #imageLiteral(resourceName: "eggs-pan"))
+        let user3 = UserIdentData(user: "Jess",userPic: #imageLiteral(resourceName: "defaultPhoto"))
+        let user4 = UserIdentData(user: "Sam",userPic: #imageLiteral(resourceName: "eggs-pan"))
+        UserData.append(user1!)
+        UserData.append(user2!)
+        UserData.append(user3!)
+        UserData.append(user4!)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
@@ -39,43 +42,20 @@ class UserTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return UserData.count
-    }
-    
-    private func orderKeys(){
-        if order.isEmpty
-        {
-        for user in UserData.keys{
-            order.append(user.user)
-            }
-        }
-        order.sort()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellidentifier = "UserTableViewCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellidentifier, for: indexPath) as! UserTableViewCell
-        orderKeys()
-        let key = findKeyObject(UserData: UserData, indexPath: indexPath)
-        cell.userNameLabel.text = key.user
-        cell.userImageView.image = key.userPic
+        let Found = UserData[indexPath.row]
+        
+        cell.userNameLabel.text = Found.user
+        cell.userImageView.image = Found.userPic
         
         return cell
     }
     
-    private func findKeyObject(UserData: [UserIdentData:[DateData]],indexPath: IndexPath) -> UserIdentData
-    {
-        var key: UserIdentData? = UserIdentData(user:"",userPic:nil)
-        for user in UserData.keys
-        {
-            if user.user == order[indexPath.row]
-            {
-                key = user
-            }
-        }
-        return key!
-    }
     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -87,8 +67,7 @@ class UserTableViewController: UITableViewController {
     {
             if editingStyle == .delete
             {
-                let key = findKeyObject(UserData: UserData, indexPath: indexPath)
-                UserData.removeValue(forKey: key)
+                UserData.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
             else if editingStyle == .insert {
@@ -96,30 +75,51 @@ class UserTableViewController: UITableViewController {
         }    
     }
  
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    private func SortUsers(){
+        for elem in UserData{
+            elem.user = elem.user.capitalized
+        }
+        UserData = UserData.sorted(by: {$0.user < $1.user})
     }
-    */
+    
+    @IBAction func unwindToUserTable (sender: UIStoryboardSegue){
+        let UserDetailViewController = sender.source as! UserDetailViewController
+        let newUser = UserDetailViewController.sendingUser
+        UserData.append(newUser)
+        SortUsers()
+        let newIndex = UserData.index(of: newUser)
+        
+        
+        tableView.insertRows(at: [IndexPath(row:newIndex!,section:0)], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let temp_User = UserData[fromIndexPath.row] //First item to move in array
+        UserData.remove(at: fromIndexPath.row)
+        UserData.insert(temp_User, at: to.row)
+        super.tableView.moveRow(at: fromIndexPath, to: to)
+    }
+ 
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
-        return true
+        return false
     }
-    */
+ 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
+        if let newViewController = segue.destination as? UserDetailViewController{
+            newViewController.UserData = UserData
+        }
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
